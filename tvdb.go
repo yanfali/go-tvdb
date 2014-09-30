34578e9dcdd4a6f0603d98feb77daa2a139c1855
Yan-Fa Li <yanfali@gmail.com>
2014-09-29 19:54:27 -0700

    Make tvdb instance configurable
    - format using gofmt
    - create a couple of constructors NewTvdb and NewTvdbWithConfig
    - make SearchSeries and GetDetail aware of TvdbConfig and Tvdb
    - add gitignore for vim tmp files

--- tvdb.go
+++ tvdb.go
@@ -2,32 +2,38 @@
 package tvdb
 
 import (
-	"fmt"
-	"errors"
-	"regexp"
-	"net/url"
-	"strings"
-	"strconv"
-	"net/http"
-	"io/ioutil"
 	"encoding/xml"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 const (
 	// TheTVDB API key.
 	API_KEY = "DECE3B6B5464C552"
 
+	// Default language
+	DEFAULT_LANGUAGE = "en"
+
+	// Protocol and Host Prefix
+	HOST = "http://thetvdb.com"
+
 	// URL used to get basic series information by name.
-	GET_SERIES_URL = "http://thetvdb.com/api/GetSeries.php?seriesname=%v"
+	GET_SERIES_URL = HOST + "/api/GetSeries.php?seriesname=%v"
 
 	// URL used to get basic series information by ID.
-	GET_SERIES_BY_ID_URL = "http://thetvdb.com/api/%v/series/%v/en.xml"
+	GET_SERIES_BY_ID_URL = HOST + "/api/%v/series/%v/%v.xml"
 
 	// URL used to get detailed series/episode information by ID.
-	GET_DETAIL_URL = "http://thetvdb.com/api/%v/series/%v/all/en.xml"
+	GET_DETAIL_URL = HOST + "/api/%v/series/%v/all/%v.xml"
 
 	// URL used for series web searches.
-	SEARCH_SERIES_URL = "http://thetvdb.com/?string=%v&searchseriesid=&tab=listseries&function=Search"
+	SEARCH_SERIES_URL = HOST + "/?string=%v&searchseriesid=&tab=listseries&function=Search"
 
 	// URL used for series web search matching.
 	SEARCH_SERIES_REGEX = `(?P<before><a href="/\?tab=series&amp;id=)(?P<seriesId>\d+)(?P<after>\&amp;lid=\d*">)`
@@ -54,65 +60,65 @@ func (pipeList *PipeList) UnmarshalXML(decoder *xml.Decoder, start xml.StartElem
 
 // Episode represents a TV show episode on TheTVDB.
 type Episode struct {
-	Id uint64 `xml:"id"`
-	CombinedEpisodeNumber string `xml:"Combined_episodenumber"`
-	CombinedSeason uint64 `xml:"Combined_season"`
-	DvdChapter string `xml:"DVD_chapter"`
-	DvdDiscId string `xml:"DVD_discid"`
-	DvdEpisodeNumber string `xml:"DVD_episodenumber"`
-	DvdSeason string `xml:"DVD_season"`
-	Director PipeList `xml:"Director"`
-	EpImgFlag string `xml:"EpImgFlag"`
-	EpisodeName string `xml:"EpisodeName"`
-	EpisodeNumber uint64 `xml:"EpisodeNumber"`
-	FirstAired string `xml:"FirstAired"`
-	GuestStars string `xml:"GuestStars"`
-	ImdbId string `xml:"IMDB_ID"`
-	Language string `xml:"Language"`
-	Overview string `xml:"Overview"`
-	ProductionCode string `xml:"ProductionCode"`
-	Rating string `xml:"Rating"`
-	RatingCount string `xml:"RatingCount"`
-	SeasonNumber uint64 `xml:"SeasonNumber"`
-	Writer PipeList `xml:"Writer"`
-	AbsoluteNumber string `xml:"absolute_number"`
-	Filename string `xml:"filename"`
-	LastUpdated string `xml:"lastupdated"`
-	SeasonId uint64 `xml:"seasonid"`
-	SeriesId uint64 `xml:"seriesid"`
-	ThumbAdded string `xml:"thumb_added"`
-	ThumbHeight string `xml:"thumb_height"`
-	ThumbWidth string `xml:"thumb_width"`
+	Id                    uint64   `xml:"id"`
+	CombinedEpisodeNumber string   `xml:"Combined_episodenumber"`
+	CombinedSeason        uint64   `xml:"Combined_season"`
+	DvdChapter            string   `xml:"DVD_chapter"`
+	DvdDiscId             string   `xml:"DVD_discid"`
+	DvdEpisodeNumber      string   `xml:"DVD_episodenumber"`
+	DvdSeason             string   `xml:"DVD_season"`
+	Director              PipeList `xml:"Director"`
+	EpImgFlag             string   `xml:"EpImgFlag"`
+	EpisodeName           string   `xml:"EpisodeName"`
+	EpisodeNumber         uint64   `xml:"EpisodeNumber"`
+	FirstAired            string   `xml:"FirstAired"`
+	GuestStars            string   `xml:"GuestStars"`
+	ImdbId                string   `xml:"IMDB_ID"`
+	Language              string   `xml:"Language"`
+	Overview              string   `xml:"Overview"`
+	ProductionCode        string   `xml:"ProductionCode"`
+	Rating                string   `xml:"Rating"`
+	RatingCount           string   `xml:"RatingCount"`
+	SeasonNumber          uint64   `xml:"SeasonNumber"`
+	Writer                PipeList `xml:"Writer"`
+	AbsoluteNumber        string   `xml:"absolute_number"`
+	Filename              string   `xml:"filename"`
+	LastUpdated           string   `xml:"lastupdated"`
+	SeasonId              uint64   `xml:"seasonid"`
+	SeriesId              uint64   `xml:"seriesid"`
+	ThumbAdded            string   `xml:"thumb_added"`
+	ThumbHeight           string   `xml:"thumb_height"`
+	ThumbWidth            string   `xml:"thumb_width"`
 }
 
 // Series represents TV show on TheTVDB.
 type Series struct {
-	Id uint64 `xml:"id"`
-	Actors PipeList `xml:"Actors"`
-	AirsDayOfWeek string `xml:"Airs_DayOfWeek"`
-	AirsTime string `xml:"Airs_Time"`
-	ContentRating string `xml:"ContentRating"`
-	FirstAired string `xml:"FirstAired"`
-	Genre PipeList `xml:"Genre"`
-	ImdbId string `xml:"IMDB_ID"`
-	Language string `xml:"Language"`
-	Network string `xml:"Network"`
-	NetworkId string `xml:"NetworkID"`
-	Overview string `xml:"Overview"`
-	Rating string `xml:"Rating"`
-	RatingCount string `xml:"RatingCount"`
-	Runtime string `xml:"Runtime"`
-	SeriesId string `xml:"SeriesID"`
-	SeriesName string `xml:"SeriesName"`
-	Status string `xml:"Status"`
-	Added string `xml:"added"`
-	AddedBy string `xml:"addedBy"`
-	Banner string `xml:"banner"`
-	Fanart string `xml:"fanart"`
-	LastUpdated string `xml:"lastupdated"`
-	Poster string `xml:"poster"`
-	Zap2ItId string `xml:"zap2it_id"`
-	Seasons map[uint64][]Episode
+	Id            uint64   `xml:"id"`
+	Actors        PipeList `xml:"Actors"`
+	AirsDayOfWeek string   `xml:"Airs_DayOfWeek"`
+	AirsTime      string   `xml:"Airs_Time"`
+	ContentRating string   `xml:"ContentRating"`
+	FirstAired    string   `xml:"FirstAired"`
+	Genre         PipeList `xml:"Genre"`
+	ImdbId        string   `xml:"IMDB_ID"`
+	Language      string   `xml:"Language"`
+	Network       string   `xml:"Network"`
+	NetworkId     string   `xml:"NetworkID"`
+	Overview      string   `xml:"Overview"`
+	Rating        string   `xml:"Rating"`
+	RatingCount   string   `xml:"RatingCount"`
+	Runtime       string   `xml:"Runtime"`
+	SeriesId      string   `xml:"SeriesID"`
+	SeriesName    string   `xml:"SeriesName"`
+	Status        string   `xml:"Status"`
+	Added         string   `xml:"added"`
+	AddedBy       string   `xml:"addedBy"`
+	Banner        string   `xml:"banner"`
+	Fanart        string   `xml:"fanart"`
+	LastUpdated   string   `xml:"lastupdated"`
+	Poster        string   `xml:"poster"`
+	Zap2ItId      string   `xml:"zap2it_id"`
+	Seasons       map[uint64][]Episode
 }
 
 // SeriesList represents a list of TV shows, often used for returning search results.
@@ -138,10 +144,32 @@ func NewSeries(data []byte) (*Series, error) {
 	return &series, nil
 }
 
+// Tvdb query handler
+type Tvdb struct {
+	ApiKey   string
+	Language string
+}
+
+// Tvdb query configuration
+type TvdbConfig struct {
+	ApiKey   string
+	Language string
+}
+
+// Tvdb with a configuration
+func NewTvdbWithConfig(config TvdbConfig) *Tvdb {
+	return &Tvdb{config.ApiKey, config.Language}
+}
+
+// Tvdb with default configuration
+func NewTvdb() *Tvdb {
+	return NewTvdbWithConfig(TvdbConfig{API_KEY, DEFAULT_LANGUAGE})
+}
+
 // Get more detail for all TV shows in a list.
-func (seriesList *SeriesList) GetDetail() (err error) {
+func (seriesList *SeriesList) GetDetail(config TvdbConfig) (err error) {
 	for seriesIndex := range seriesList.Series {
-		if err = seriesList.Series[seriesIndex].GetDetail(); err != nil {
+		if err = seriesList.Series[seriesIndex].GetDetail(config); err != nil {
 			return
 		}
 	}
@@ -150,8 +178,8 @@ func (seriesList *SeriesList) GetDetail() (err error) {
 }
 
 // Get more detail for a TV show, including information on it's episodes.
-func (series *Series) GetDetail() (err error) {
-	response, err := http.Get(fmt.Sprintf(GET_DETAIL_URL, API_KEY, strconv.FormatUint(series.Id, 10)))
+func (series *Series) GetDetail(config TvdbConfig) (err error) {
+	response, err := http.Get(fmt.Sprintf(GET_DETAIL_URL, config.ApiKey, strconv.FormatUint(series.Id, 10), config.Language))
 
 	if err != nil {
 		return
@@ -204,8 +232,8 @@ func GetSeries(name string) (seriesList SeriesList, err error) {
 }
 
 // Get a TV series by ID.
-func GetSeriesById(id uint64) (series Series, err error) {
-	response, err := http.Get(fmt.Sprintf(GET_SERIES_BY_ID_URL, API_KEY, id))
+func (mytvdb *Tvdb) GetSeriesById(id uint64) (series Series, err error) {
+	response, err := http.Get(fmt.Sprintf(GET_SERIES_BY_ID_URL, mytvdb.ApiKey, id, mytvdb.Language))
 
 	if err != nil {
 		return
@@ -236,7 +264,7 @@ func GetSeriesById(id uint64) (series Series, err error) {
 
 // Search for TV shows by name, using the more sophisticated search on TheTVDB's homepage.
 // This is the recommended search method.
-func SearchSeries(name string, maxResults int) (seriesList SeriesList, err error) {
+func (mytvdb *Tvdb) SearchSeries(name string, maxResults int) (seriesList SeriesList, err error) {
 	response, err := http.Get(fmt.Sprintf(SEARCH_SERIES_URL, url.QueryEscape(name)))
 
 	if err != nil {
@@ -265,7 +293,7 @@ func SearchSeries(name string, maxResults int) (seriesList SeriesList, err error
 			return
 		}
 
-		series, err = GetSeriesById(seriesId)
+		series, err = mytvdb.GetSeriesById(seriesId)
 
 		if err != nil {
 			// Some series can't be found, so we will ignore these.
